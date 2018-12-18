e0a913f8dfcfa86e1e35ed3d91db1f81d31c16d9
USER <USER@192.168.3.16>
2018-12-18 16:12:01 +0900

    add:goodscore/highestscore

--- StudentManager/WebContent/WEB-INF/index.jsp
+++ StudentManager/WebContent/WEB-INF/index.jsp
@@ -4,84 +4,96 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" type="text/css" href="./css/student.css"/>
-  <title>学员管理</title>
-<script src="http://cdn.static.runoob.com/libs/jquery/1.10.2/jquery.min.js"></script>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="./css/student.css"/>
+	<!-- jQuery -->
+<script type="text/javascript" charset="utf8"
+	src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
+<!-- DataTables -->
+<script type="text/javascript" charset="utf8"
+	src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+	<title>学员管理</title>
+	<script src="http://cdn.static.runoob.com/libs/jquery/1.10.2/jquery.min.js"></script>
 
 	<script type="text/javascript">
-		$(document).ready(function(){
-			  $("#display").click(function(){
-				  $("#displayGood").submit();
-				  }
-		}
-		});
-		  });
+	$(function(){
+		  $("#goodScorebtn").click(function(){
+				$("#displayGoodScore").submit();
+			});
 
+		  $("#maxScorebtn").click(function(){
+				$("#displayMaxScore").submit();
+			});
+	});
 	</script>
 </head>
 
 <body>
-<img src="./images/header.jpg" />
-${msg}
-<h1 align="center">学员信息管理</h1>
-
-<div id="all_comm" class="all"  >
-  <h2 align="center">学员信息一览</h2>
-  <table id="items" cellspacing="0px" cellspadding="0px" align="center" width="500px">
-	<tr>
-		<th>ID</td>
-		<th width="80px">姓名</th>
-		<th width="150px">出生年月</th>
-		<th width="40px">年龄</th>
-		<th width="80px">分数</th>
-		<th width="20px">ClassId</th>
-	</tr>
-
-    <c:forEach items="${students}" var="student" >
-	    <tr style="color:blue">
-			<td id="id${student.id }">${student.id}</td>
-			<td id="name${student.id }">${student.name}</td>
-			<td id="birthday${student.id}">${student.birthday}</td>
-			<td id="age${student.id }">${student.age}</td>
-			<td id="score${student.id}">${student.score}</td>
-			<td id="classid${student.id}">${student.classid}</td>
-
-		 </tr>
-	 </c:forEach>
-  </table>
-</div>
-
-	<div id="add_comm" class="all" align="center">
-	  <h2>查找学员</h2>
-	  <form action="queryByName" method="post" >
-		<input type="text" placeholder="姓名" name="name" >
-		<input type="submit" value="查找学员" >
-	  </form>
-	  <form action="queryById" method="post" >
-		<input type="text" placeholder="ID" name="id" >
-		<input type="submit" value="查找学员" >
-	  </form>
-
-	  <h2 id="edit_title">添加学员</h2>
-		<form  id="addForm" action="add" method="post" >
-		<input id="addid" type="text" placeholder="ID" name="id" />
-		<input id="addname" type="text" placeholder="姓名" name="name" />
-		<input id="addbirthday" type="text" placeholder="出生年月" name="birthday" />
-		<input id="addage" type="text" placeholder="年龄" name="age" />
-		<input id="addscore" type="text" placeholder="分数" name="score" />
-		<input id="addclassid" type="text" placeholder="ClassId" name="classid" />
-		<input type="button" value="添加" onClick="addCheckForm()"/>
-	  </form>
-	</div>
 
 	<div align="center">
-		<h2>成績優秀者</h2>
-		<form id="displayGood" action="displayGoodController" method="post">
-			<button id="display" type="button">表示</button>
-		</form>
+	<img src="./images/header.jpg" alt="first develope" title="first develope"/>
 	</div>
+
+	<h1 align="center">学生情報管理システム</h1>
+
+	<div id="all_comm" class="all"  >
+	  <h2 align="center">情報一覧</h2>
+	  <table id="items" cellspacing="0px" cellspadding="0px" align="center" width="500px">
+		<tr>
+			<th>ID</td>
+			<th width="80px">Name</th>
+			<th width="150px">Birthday</th>
+			<th width="40px">Age</th>
+			<th width="80px">Score</th>
+			<th width="20px">ClassId</th>
+		</tr>
+
+	    <c:forEach items="${students}" var="student" >
+		    <tr style="color:blue">
+				<td id="id${student.id }">${student.id}</td>
+				<td id="name${student.id }">${student.name}</td>
+				<td id="birthday${student.id}">${student.birthday}</td>
+				<td id="age${student.id }">${student.age}</td>
+				<td id="score${student.id}">${student.score}</td>
+				<td id="classid${student.id}">${student.classid}</td>
+			 </tr>
+		 </c:forEach>
+	  </table>
+	</div><br><br>
+
+	<div align="center">
+		<form id="displayGoodScore" action="goodScore" method="post">
+			<button id="goodScorebtn" type="button">成績優秀者を表示</button>
+		</form><br>
+		<form id="displayMaxScore" action="maxScore" method="post">
+			<button id="maxScorebtn" type="button">成績最大値を表示</button>
+		</form>
+	</div><br><br>
+
+	<div id="add_comm" class="all" align="center">
+	<h2>学生を検索</h2>
+	<form action="queryByName" method="post" >
+		<input type="text" placeholder="名前" name="name" >
+		<input type="submit" value="名前で検索" ><br><br>
+	</form>
+	  <form action="queryById" method="post" >
+		<input type="text" placeholder="ID" name="id" >
+		<input type="submit" value="IDで検索" >
+	  </form><br><br>
+
+	  <h2>学生を追加</h2>
+	  	<form  id="addForm" action="add" method="post" >
+		<input id="addid" type="text" placeholder="ID" name="id" />
+		<input id="addname" type="text" placeholder="名前" name="name" />
+		<input id="addbirthday" type="text" placeholder="生年月日" name="birthday" />
+		<input id="addage" type="text" placeholder="年齢" name="age" />
+		<input id="addscore" type="text" placeholder="成績" name="score" />
+		<input id="addclassid" type="text" placeholder="ClassId" name="classid" />
+		<input type="button" value="追加" onClick="addCheckForm()"/>
+	  </form>
+	</div><br><br>
 
 </body>
 </html>
--- StudentManager/src/com/student/controller/StudentController.java
+++ StudentManager/src/com/student/controller/StudentController.java
@@ -1,7 +1,5 @@
 package com.student.controller;
 
-
-
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -27,6 +25,11 @@ import com.teacher.dao.TeacherDao;
 @Controller
 public class StudentController {
 
+	/**
+	 * すべての学生の情報を表示する。
+	 * @param model
+	 * @return index.jsp
+	 */
 	@RequestMapping(value = "/all")
 	public String queryAll(Model model) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
@@ -36,102 +39,12 @@ public class StudentController {
 		return "index";
 	}
 
-
-	@RequestMapping(value = "/teacherall")
-	public String queryTeacherAll(Model model) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		TeacherDao dao = (TeacherDao) context.getBean("teacherdao");
-		model.addAttribute("teachers", dao.queryTeacherAll());
-		return "teacher";
-	}
-
-
-	@RequestMapping(value = "/queryByName")
-	public String queryByName(String teachername, Model model) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		//从ioc容器中获取dao
-		TeacherDao dao = (TeacherDao) context.getBean("teacherdao");
-		model.addAttribute("teachers", dao.queryByName(teachername));
-		return "teacher";
-	}
-
-	@RequestMapping(value = "/queryById")
-	public String queryById(String id, Model model) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		TeacherDao dao = (TeacherDao) context.getBean("teacherdao");
-		model.addAttribute("teachers", dao.queryById(id));
-		return "teacher";
-	}
-
-	@RequestMapping(value = "/add")
-	public String Addstudent(String name, String birthday, String age, String score, String classid, Model model) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		StudentDao dao = (StudentDao) context.getBean("dao");
-		Student student = new Student();
-		student.setName(name);
-		student.setBirthday(birthday);
-		student.setAge(Integer.valueOf(age));
-		student.setScore(Double.parseDouble(score));
-		student.setClassid(Integer.valueOf(classid));
-		boolean result = dao.addStu(student);
-		if (result) {
-			model.addAttribute("msg", "<script>alert('添加成功！')</script>");
-		} else {
-			model.addAttribute("msg", "<script>alert('添加失敗！')</script>");
-		}
-		model.addAttribute("students", dao.queryAll());
-		return "index";
-	}
-
-	@RequestMapping(value = "/addteacher")
-	public String Addteacher(String teachername, String course, Model model) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		TeacherDao dao = (TeacherDao) context.getBean("teacherdao");
-		Teacher teacher = new Teacher();
-		teacher.setTeachername(teachername);
-		teacher.setCourse(course);
-		boolean result = dao.addTea(teacher);
-		if (result) {
-			model.addAttribute("msg", "<script>alert('添加成功！')</script>");
-		} else {
-			model.addAttribute("msg", "<script>alert('添加失敗！')</script>");
-		}
-		model.addAttribute("teachers", dao.queryTeacherAll());
-		return "teacher";
-	}
-
-	@RequestMapping(value = "/updateteacher")
-	public String updateTeacher(String id, String teachername, String course, Model model) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		TeacherDao dao = (TeacherDao) context.getBean("teacherdao");
-		Teacher teacher = new Teacher();
-		teacher.setId(Integer.parseInt(id));
-		teacher.setTeachername(teachername);
-		teacher.setCourse(course);
-		boolean result = dao.updateTeacher(teacher);
-		if (result) {
-			System.out.println("update成功");
-		} else {
-			System.out.println("update失敗");
-		}
-		model.addAttribute("teachers", dao.queryTeacherAll());
-		return "teacher";
-	}
-
-	@RequestMapping(value = "/deleteTeacherById")
-	public String deleteById(String id, Model model) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		TeacherDao dao = (TeacherDao) context.getBean("teacherdao");
-		boolean result = dao.deleteTea(Integer.parseInt(id));
-		if (result) {
-			System.out.println("消除成功");
-		} else {
-			System.out.println("消除失敗");
-		}
-		model.addAttribute("teachers", dao.queryTeacherAll());
-		return "teacher";
-	}
-
+	/**
+	 * ログイン画面で、アドレスを変えないままボタンを押して指定の内容を表示するAjax機能を実現する。
+	 * 実現方法：ログイン画面の「read」ボタンを押す。
+	 * @param request
+	 * @param response
+	 */
 	@RequestMapping(value = "/myajax", produces = "application/json;charset=UTF-8")
 	public void getAjax(HttpServletRequest request, HttpServletResponse response) {
 		JSONObject obj = new JSONObject();
@@ -139,9 +52,9 @@ public class StudentController {
 		StudentDao studao = (StudentDao) context.getBean("dao");
 		List<Student> studentList = new ArrayList<Student>();
 		studentList = (ArrayList<Student>) studao.queryAll();
-		JSONArray data = new JSONArray(); //  List
+		JSONArray data = new JSONArray(); //List機能に相当する
 		for (Student stu : studentList) {
-			JSONObject jsonObject = new JSONObject(); // Map
+			JSONObject jsonObject = new JSONObject(); //Map機能に相当する。
 			jsonObject.put("id", stu.getId());
 			jsonObject.put("name", stu.getName());
 			jsonObject.put("birthday", stu.getBirthday());
@@ -160,6 +73,12 @@ public class StudentController {
 		}
 	}
 
+	/**
+	 * 学生と先生の情報をIDを通じて一つのテーブルに表示する。
+	 * 実現方法：アドレスの末尾を変更。
+	 * @param model
+	 * @return stutea
+	 */
 	@RequestMapping(value = "/stuteaall")
 	public String queryStudentTeacherAll(Model model) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
@@ -168,15 +87,11 @@ public class StudentController {
 		return "stutea";
 	}
 
-	@RequestMapping(value = "/displayGood")
-	public String displayGoodScore(Model model) {
-		List<Student> studentList = getStudentList();
-		BigDecimal average = getAverAge(studentList);
-		List<Student> returnList = getReturnList(studentList, average);
-		model.addAttribute("students", returnList);
-		return "index";
-	}
-
+	/**
+	 * 以下四つの方法を合わせて、成績平均値を超えた学生を取り出して表示する。
+	 * その一：全員の情報を取り出して、「studentList」というリストに表示する。
+	 * @return studentList
+	 */
 	private List<Student> getStudentList() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		StudentDao studao = (StudentDao) context.getBean("dao");
@@ -185,22 +100,123 @@ public class StudentController {
 		return studentList;
 	}
 
-	private BigDecimal getAverAge(List<Student> studentList) {
-		BigDecimal average = BigDecimal.ZERO;
-		for (Student student : studentList) {
-			average.add(new BigDecimal(student.getScore()));
+	/**
+	 * その二：全員の成績の平均値を求める。（コメントの内容は、まだ習っていない別の先生の書き方）
+	 * @param stuList
+	 * @return averageScore
+	 */
+//	private BigDecimal getAverScore(List<Student> studentList) {
+//		BigDecimal average = BigDecimal.ZERO;
+//		for (Student student : studentList) {
+//			average.add(new BigDecimal(student.getScore()));
+//		}
+//		//System.out.println(average.divide(new BigDecimal(studentList.size())));
+//		return average.divide(new BigDecimal(studentList.size()));
+	private double getAverageScore(List<Student> stuList) {
+		double sum = 0;
+		for(Student stu:stuList) {
+			sum += stu.getScore();
 		}
-		return average.divide(new BigDecimal(studentList.size()));
+		double averageScore = sum / stuList.size();
+		System.out.print(averageScore);
+		return averageScore;
 	}
 
-	private List<Student> getReturnList(List<Student> studentList, BigDecimal average) {
-		List<Student> returnList = new ArrayList<>();
-		for (Student student : studentList) {
-			if (new BigDecimal(student.getScore()).compareTo(average) > 0) {
-				returnList.add(student);
+	/**
+	 * その三：成績が平均値より高い学生の情報を取り出して、「returnGoodScoreList」というリストに表示する。
+	 * （コメントの内容は、まだ習っていない別の先生の書き方）
+	 * @param stuList
+	 * @param avrage
+	 * @return returnGoodScoreList
+	 */
+//	private List<Student> getReturnList(List<Student> studentList, BigDecimal average) {
+//		List<Student> returnList = new ArrayList<>();
+//		for (Student student : studentList) {
+//			if (new BigDecimal(student.getScore()).compareTo(average) >0) {
+//				returnList.add(student);
+//			}
+//		}
+//		return returnList;
+//	}
+	private List<Student> getGoodScoreReturnList(List<Student> stuList,double averageScore) {
+		List<Student> returnGoodScoreList = new ArrayList<>();
+		for (Student stu:stuList) {
+			if (averageScore < stu.getScore()) {
+				returnGoodScoreList.add(stu);
 			}
 		}
-		return returnList;
+		return returnGoodScoreList;
+	}
+
+	/**
+	 * その四：上記のリスト「returnList」をモデルの形で、フロントの「index.jsp」ファイルに返す。
+	 * （コメントの内容は、まだ習っていない別の先生の書き方）
+	 * @param model
+	 * @return
+	 */
+//	@RequestMapping(value = "/displayGoodScore")
+//	public String displayGoodScore(Model model) {
+//		List<Student> studentList = getStudentList();
+//		BigDecimal average = getAverageScore(studentList);
+//		List<Student> returnList = getReturnList(studentList, average);
+//		model.addAttribute("students", returnList);
+//		return "index";
+//	}
+	@RequestMapping(value = "/goodScore")
+	public String displayGoodScore(Model model) {
+		List<Student> studentList = getStudentList();
+		double averageScore = getAverageScore(studentList);
+		List<Student> returnGoodScoreList = getGoodScoreReturnList(studentList, averageScore);
+		model.addAttribute("students", returnGoodScoreList);
+		return "index";
+	}
+
+	/**
+	 * 以下四つの方法を合わせて、成績が最大値に該当する学生を取り出して表示する。
+	 * その一（上と被るゆえ省略）：全員の情報を取り出して、「studentList」というリストに表示する。
+	 * その二：全員の成績の最大値を求める。
+	 * @param stuList
+	 * @return double
+	 */
+	private double getMaxScore(List<Student> stuList) {
+		double maxScore = 0;
+		for(Student stu:stuList) {
+			if(maxScore < stu.getScore()) {
+				maxScore = stu.getScore();
+			}
+		}
+		System.out.print(maxScore);
+		return maxScore;
+	}
+
+	/**
+	 * その三：成績が最大値と等しい学生の情報を、「returnMaxScoreList」というリストに表示する。
+	 * @param stuList
+	 * @param avrage
+	 * @return returnMaxScoreList
+	 */
+	private List<Student> getMaxScoreReturnList(List<Student> stuList,double maxScore) {
+		List<Student> returnMaxScoreList = new ArrayList<>();
+		for (Student stu:stuList) {
+			if (maxScore == stu.getScore()) {
+				returnMaxScoreList.add(stu);
+			}
+		}
+		return returnMaxScoreList;
+	}
+
+	/**
+	 * その四：上記のリスト「returnMaxScoreList」をモデルの形で、フロントの「index.jsp」ファイルに返す。
+	 * @param model
+	 * @return index.jsp
+	 */
+	@RequestMapping(value = "/maxScore")
+	public String displayMaxScore(Model model) {
+		List<Student> studentList = getStudentList();
+		double maxScore = getMaxScore(studentList);
+		List<Student> returnMaxScoreList = getMaxScoreReturnList(studentList, maxScore);
+		model.addAttribute("students", returnMaxScoreList);
+		return "index";
 	}
 
 }
